34fe56d185a1d2dc298b4369119609444c7988f9
hardi-yansah <hardi.yansah97@gmail.com>
2023-12-01 16:56:00 +0700

    #4 Set up Passport

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const { ObjectID } = require('mongodb');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
@@ -18,6 +32,16 @@ app.route('/').get((req, res) => {
     title: 'Hello',
     message: 'Please log in'
   });
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDatabase.findOne({_id: newObject(id)}, (err, doc) => {
+    done(null, null);
+  // });
 });
 
 const PORT = process.env.PORT || 3000;
