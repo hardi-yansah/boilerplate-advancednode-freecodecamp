390dc1235aa2c37bfc7c827868bd596ff6a2d624
hardi-yansah <hardi.yansah97@gmail.com>
2023-12-05 15:39:33 +0700

    fix:#4 a logout

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
 
-  app.route('/').get(async (req, res) => {
+  app.route('/').get( async (req, res) => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please log in',
@@ -44,15 +44,26 @@ myDB(async client => {
   });
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
+    res.render('profile', { username: req.user.username });
+  });
+
+  app.route('/logout').get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
 
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {
       console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
       return done(null, user);
     });
   }));
@@ -66,12 +77,10 @@ myDB(async client => {
       done(null, doc);
     });
   });
+
 }).catch(e => {
-  app.route('/').get(async (req, res) => {
-    res.render('index', {
-      title: e,
-      message: e
-    });
+  app.route('/').get( async (req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
   });
 });
 
@@ -79,23 +88,10 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/')
+  res.redirect('/');
 };
 
-app.route('/logout')
-  .get((req, res) => {
-    req.logout();
-    res.redirect('/');
-  });
-
-app.use((req, res, next) => {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
