c4f6835dc0e9477e08c2901d5bf263a8111b2fd9
hardi-yansah <hardi.yansah97@gmail.com>
2024-05-25 01:23:08 +0700

    fix: #5 Implementation of Social Authentication

--- routes.js
+++ routes.js
@@ -12,12 +12,6 @@ module.exports = function (app, myDataBase) {
     });
   });
 
-  app.route('/auth/github').get(passport.authenticate('github'));
-  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }),
-    (req, res) => {
-      res.redirect('/profile');
-    });
-
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
@@ -61,6 +55,11 @@ module.exports = function (app, myDataBase) {
       res.redirect('/profile');
     }
   );
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  });
 
   app.use((req, res, next) => {
     res.status(404)
