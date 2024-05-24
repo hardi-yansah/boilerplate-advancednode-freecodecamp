be771856d22d0bf027dad0e747878000ce9ac4ee
hardi-yansah <hardi.yansah97@gmail.com>
2024-05-24 20:41:02 +0700

    fix: #2 Implementation of Social Authentication

--- routes.js
+++ routes.js
@@ -1,39 +1,36 @@
-const passport = require("passport");
-const bcrypt = require("bcrypt");
-const GitHubStrategy = require('passport-github').Strategy;
-require("dotenv").config();
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 
 module.exports = function (app, myDataBase) {
-  app.route("/").get((req, res) => {
-    res.render("index", {
-      title: "Connected to Database",
-      message: "Please log in",
+  app.route('/').get((req, res) => {
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in',
       showLogin: true,
-      showRegistration: true,
-      showSocialAuth: true
+      showRegistration: true
     });
   });
 
-  app.route("/login").post(passport.authenticate("local", { failureRedirect: "/" }), (req, res) => {
-    res.redirect("/profile");
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
   });
 
-  app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile", { username: req.user.username });
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    res.render('profile', { username: req.user.username });
   });
 
-  app.route("/logout").get((req, res) => {
+  app.route('/logout').get((req, res) => {
     req.logout();
-    res.redirect("/");
+    res.redirect('/');
   });
 
-  app.route("/register").post((req, res, next) => {
+  app.route('/register').post((req, res, next) => {
     const hash = bcrypt.hashSync(req.body.password, 12);
     myDataBase.findOne({ username: req.body.username }, (err, user) => {
       if (err) {
         next(err);
       } else if (user) {
-        res.redirect("/");
+        res.redirect('/');
       } else {
         myDataBase.insertOne({
           username: req.body.username,
@@ -41,7 +38,7 @@ module.exports = function (app, myDataBase) {
         },
           (err, doc) => {
             if (err) {
-              res.redirect("/");
+              res.redirect('/');
             } else {
               // The inserted document is held within
               // the ops property of the doc
@@ -52,55 +49,22 @@ module.exports = function (app, myDataBase) {
       }
     });
   },
-    passport.authenticate("local", { failureRedirect: "/" }),
+    passport.authenticate('local', { failureRedirect: '/' }),
     (req, res, next) => {
-      res.redirect("/profile");
+      res.redirect('/profile');
     }
   );
 
-  app.route("/auth/github").get(passport.authenticate("github"));
-  app.route("/auth/github/callback").get(passport.authenticate("github", { failureRedirect: "/" }), (req, res) => {
-    res.redirect("/profile");
-  });
-
   app.use((req, res, next) => {
     res.status(404)
-      .type("text")
-      .send("Not Found");
+      .type('text')
+      .send('Not Found');
   });
 };
-
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "https://3000-hardiyansah-boilerplate-ayqdbgwglzn.ws-us114.gitpod.io/auth/github/callback"
-},
-  function (accessToken, refreshToken, profile, done) {
-    myDataBase.findOneAndUpdate(
-      { id: profile.id },
-      {
-        $setOnInsert: {
-          id: profile.id,
-          name: profile.displayName || "John Doe",
-          photo: profile.photos[0].value || "",
-          email: profile.emails[0].value || "No public email",
-          created_on: new Date(),
-          provider: profile.provider || ""
-        },
-        $set: {
-          last_login: new Date()
-        },
-      },
-      { upsert: true, new: true },
-      (err, doc) => {
-        return done(null, doc.value);
-      });
-  }
-));
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect("/");
+  res.redirect('/');
 };
