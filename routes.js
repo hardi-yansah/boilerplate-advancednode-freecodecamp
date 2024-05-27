3b09bf160b4d1389c877f1900779f3bcc23453fd
hardi-yansah <hardi.yansah97@gmail.com>
2024-05-27 13:11:02 +0700

    fix: #2 Set up the Environment

--- routes.js
+++ routes.js
@@ -58,6 +58,7 @@ module.exports = function (app, myDataBase) {
 
   app.route('/auth/github').get(passport.authenticate('github'));
   app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    req.session.user_id = req.user.id;
     res.redirect('/chat');
   });
 
